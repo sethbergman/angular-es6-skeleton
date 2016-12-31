71b116213d23bbadc589a0baf24a27f74ab59412
Seth Bergman <seth.atxwebs@gmail.com>
2016-12-31 02:24:40 -0600

    deploy

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.join(__dirname, '..', 'dist'),
+    path: __dirname + '/dist',
     filename: 'bundle.js',
   },
   plugins: [
