fc011b3f79dd17636a070eebf989aaf40271b543
Seth Bergman <seth.atxwebs@gmail.com>
2016-12-31 03:45:22 -0600

    deploy

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -12,6 +12,7 @@ module.exports = webpackMerge(commonConfig, {
   debug: false,
   devtool: 'source-map',
   output: {
+    context: '/'
     path: path.join(__dirname, '..', 'dist'),
     filename: '[name].[chunkhash].bundle.js',
     sourceMapFilename: '[name].[chunkhash].bundle.map',
