8b39fa5aab05d04a566ed82152e415fb057c41c2
Seth Bergman <seth.atxwebs@gmail.com>
2016-12-31 02:59:59 -0600

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,42 @@
-switch (process.env.NODE_ENV) {
-  case 'prod':
-    module.exports = require('./config/webpack.prod');
-    break;
-  case 'dev':
-  default:
-    module.exports = require('./config/webpack.dev');
+// switch (process.env.NODE_ENV) {
+//   case 'prod':
+//     module.exports = require('./config/webpack.prod');
+//     break;
+//   case 'dev':
+//   default:
+//     module.exports = require('./config/webpack.dev');
+// }
+
+const webpack = require('webpack');
+
+var config = {
+  context: __dirname + '/src',
+  entry: './app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      ON_TEST: process.env.NODE_ENV === 'test'
+    })
+  ],
+
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'ng-annotate!babel', exclude: /node_modules/},
+      {test: /\.html$/, loader: 'raw', exclude: /node_modules/},
+      {test: /\.css$/, loader: 'style!css', exclude: /node_modules/},
+      {test: /\.styl$/, loader: 'style!css!stylus', exclude: /node_modules/}
+    ]
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.output.path = __dirname + '/dist';
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  config.devtool = 'source-map';
 }
+
+module.exports = config;
